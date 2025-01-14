c0526b691f6d7baece987ed33c27dda1350ea406
Usman Saeed <uusaeed749@gmail.com>
2025-01-14 20:33:32 +0500

    removed unused imports

--- src/components/projects/ProjectSingle.jsx
+++ src/components/projects/ProjectSingle.jsx
@@ -1,5 +1,4 @@
 import { motion } from 'framer-motion';
-import { Link } from 'react-router-dom';
 
 const ProjectSingle = ({ title, category, image, link }) => {
 	const handleClick = () => {
--- src/components/shared/AppFooter.jsx
+++ src/components/shared/AppFooter.jsx
@@ -1,9 +1,6 @@
 import {
 	FiGithub,
-	FiTwitter,
 	FiLinkedin,
-	FiGlobe,
-	FiYoutube,
 } from 'react-icons/fi';
 import AppFooterCopyright from './AppFooterCopyright';
 
